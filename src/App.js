e2469832bdd499721a9d3d0c0a416c37efdc512d
annkim7@github.com <ilc7748@gmail.com>
2021-12-23 16:06:06 +0900

    get motion

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import './style.scss';
 import Slider from './slider.js';
 import Modal from './modal.js';
+import Motion from './motion.js';
 import $ from "jquery";
 
 
@@ -23,6 +24,32 @@ function App() {
             <section className="section section01">
                 <div className="main-visual">
                     <Slider pop={modal} setPop={modalFunc} num={num} setNum={numFunc}/>
+                </div>
+
+                <Motion />
+            </section>
+
+            <section class="section section02">
+                <div class="left-side">
+                    <h3 class="title">Education</h3>
+                    <div class="edu-con">
+                        <p>이 사이트에 사용된 기술을 정리해 보았습니다. 디자인과 퍼블리싱을 넘어 프론트엔드로 기술 확장을 도모하여 보다 나은 기술로 멋진 웹페이지를 만들어보는 것이 목표입니다. 현재에 안주하지 않고 꾸준하게 발전하기 위해 배움을 놓지 않고 있습니다.</p>
+                        <div class="edu-button">
+                            <a class="github" traget="_blank" href="https://github.com/annkim7"></a>
+                            <a class="figma" traget="_blank" href="#"></a>
+                            <a class="notion" traget="_blank" href="https://www.notion.so/N-Kim-a242915d7ffa463596860036bf7db6d5"></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="right-side">
+                    <div class="edu-box">
+                        <div class="edu">Javascript</div>
+                        <div class="edu">SVG</div>
+                        <div class="edu">Tweenmax</div>
+                        <div class="edu">React</div>
+                        <div class="edu">getJSON</div>
+                        <div class="edu">Scss</div>
+                    </div>
                 </div>
             </section>
             
